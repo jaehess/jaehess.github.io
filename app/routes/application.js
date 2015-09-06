2fc91333cf27960eed301e5c4549b569eeb837fb
Jae Hess <jae@apple.com>
2015-09-05 19:19:47 -0700

    Promise style action

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,17 +5,29 @@ const {PARTICLE} = config;
 
 export default Ember.Route.extend({
 
+  sendParams(params) {
+    let url = "https://api.spark.io/%@/devices/%@/garage".fmt(PARTICLE.apiVersion, PARTICLE.deviceId);
+
+    console.log('sendParams', params);
+    console.log('sendParams', url);
+
+    return ajax({
+      data: {
+        access_token: PARTICLE.accessToken,
+        params: params
+      },
+      method: 'POST',
+      url: url
+    });
+  },
+
   actions: {
     press() {
-      let url = "https://api.spark.io/%@/devices/%@/garage";
-      ajax({
-        data: {
-          access_token: PARTICLE.accessToken,
-          params: 'door'
-        },
-        method: 'POST',
-        url: url.fmt(PARTICLE.apiVersion, PARTICLE.deviceId)
-      })
+      this.sendParams('open').then((response) => {
+        if (response.return_value === 1) {
+          this.sendParams('close');
+        }
+      });
     }
   }
 
